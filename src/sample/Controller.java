ddd1b8d47cc8ffdeb0a0c06a3a40a18e3707118f
yurykotov <iam@kotov.tech>
2018-02-13 23:51:01 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class Controller {
+    public Label test;
+
+    public void printTest(ActionEvent actionEvent) {
+        test.setText("test...");
+    }
+}
